53b53e7347aff88a53eb2b4e561d75c7070e5b68
rohanverma445 <rohan.vermaji2003@gmail.com>
2026-01-28 10:23:24 +0530

    Add deployment config: package.json and dynamic port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const DB_FILE = path.join(__dirname, 'db.json');
 
 // Initial DB state
